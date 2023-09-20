184f4161b2abfc8a1438d86ba40479117e5abe52
Marwa Ahmed Galal <marwa2ahmad@gmail.com>
2023-09-20 13:36:43 +0300

    change the Link of login to Login component in the homw page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Login from "../Login";
 const Home: React.FC = () => (
   <div>
-    <Link to="login">to</Link>
+    <Login />
   </div>
 );
 
