d98604f14840e4117b6aa1ca66d40393a850fb0e
Marwa Ahmed Galal <marwa2ahmad@gmail.com>
2023-09-20 13:41:06 +0300

    Link of login

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Login from "../Login";
+import { Link } from "react-router-dom";
 const Home: React.FC = () => (
   <div>
-    <Login />
+    <Link to="login">to</Link>
   </div>
 );
 
